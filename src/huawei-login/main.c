fb19d0250a55ca2df05ca99fd93d743f37b1b640
Lisna Qonita Baktisari <harimubikin@gmail.com>
2026-07-20 03:25:38 +0700

    Create main.c

---
+++ src/huawei-login/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("=====================================\n");
+    printf(" Huawei Modem Backend\n");
+    printf(" Version : 0.1.0\n");
+    printf(" Target  : OpenWrt 24.10+\n");
+    printf(" Status  : Development\n");
+    printf("=====================================\n");
+
+    return 0;
+}
