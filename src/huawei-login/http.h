b6fbe102b9e8bd798fabd3c613586a2899392683
Lisna Qonita Baktisari <harimubikin@gmail.com>
2026-07-20 03:36:59 +0700

    Update http.h

--- src/huawei-login/http.h
+++ src/huawei-login/http.h
@@ -1,16 +1,16 @@
 #ifndef HTTP_H
 #define HTTP_H
 
-#include <stddef.h>
-
-#define HTTP_OK 0
-#define HTTP_ERROR -1
+#include <curl/curl.h>
 
 typedef struct {
-    int status;
+
+    long status;
+
     char *body;
-    char *cookie;
-    char *token;
+
+    size_t size;
+
 } http_response_t;
 
 int http_init(void);
@@ -20,14 +20,8 @@ int http_get(
     http_response_t *resp
 );
 
-int http_post(
-    const char *url,
-    const char *content,
-    const char *token,
-    const char *cookie,
+void http_free(
     http_response_t *resp
 );
-
-void http_free(http_response_t *resp);
 
 #endif
