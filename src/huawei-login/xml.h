ffdec01f50159e20bc5d582898661b575d1dfc62
Lisna Qonita Baktisari <harimubikin@gmail.com>
2026-07-20 03:39:03 +0700

    Update xml.h

--- src/huawei-login/xml.h
+++ src/huawei-login/xml.h
@@ -1,6 +1,20 @@
 #ifndef XML_H
 #define XML_H
 
+#include <stddef.h>
+
 int xml_init(void);
+
+/*
+ * Mengambil isi tag XML.
+ * Contoh:
+ * xml_get(xml, "TokInfo", buffer, sizeof(buffer));
+ */
+int xml_get(
+    const char *xml,
+    const char *tag,
+    char *buffer,
+    size_t size
+);
 
 #endif
